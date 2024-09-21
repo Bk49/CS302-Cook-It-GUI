e5a586da1aede2b7226daec6bf91993160f42a30
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-09-21 20:33:29 +0800

    Add authorization header injection code to remove the need to configure Authorization header multiple times

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { InternalAxiosRequestConfig } from "axios";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import { graphQL } from "../axios/client";
 import AppBar from "../components/common/nav/AppBar";
 
 interface RootLayoutProps {
@@ -7,6 +10,19 @@ interface RootLayoutProps {
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({}) => {
+    const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            const configureInstance = async (
+                config: InternalAxiosRequestConfig
+            ) => {
+                config.headers.Authorization = `Bearer ${await getAccessTokenSilently()}`;
+                return config;
+            };
+            graphQL.interceptors.request.use(configureInstance);
+        }
+    }, [isAuthenticated]);
     return (
         <>
             <AppBar />
