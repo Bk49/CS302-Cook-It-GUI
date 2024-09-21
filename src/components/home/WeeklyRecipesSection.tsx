b3268ff63cd8fc61c2b405d29a90e666af9a5fc4
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-09-21 14:47:56 +0800

    Create Weekly Recipes Section for Home Page

--- src/components/home/WeeklyRecipesSection.tsx
+++ src/components/home/WeeklyRecipesSection.tsx
@@ -1,9 +1,15 @@
 import { Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import RecipeItemCard from "../common/card/RecipeItemCard";
+import useWeeklyRecipes from "../../custom-hooks/react-query/weekly/useWeeklyRecipes";
+import { useNavigate } from "react-router-dom";
 
 interface WeeklyRecipeSectionProps {}
 
 const WeeklyRecipeSection: React.FC<WeeklyRecipeSectionProps> = ({}) => {
+    const { data: recipes } = useWeeklyRecipes();
+    const navigate = useNavigate()
+
     return (
         <Container maxWidth="xl" sx={{ mt: 4 }}>
             <Grid container gap={3}>
@@ -20,15 +26,24 @@ const WeeklyRecipeSection: React.FC<WeeklyRecipeSectionProps> = ({}) => {
                             </Typography>
                         </Grid>
                         <Grid item>
-                            <Typography>{"View All Recipes >"}</Typography>
+                            <Typography onClick={() => navigate("/recipe")}>{"View All Recipes >"}</Typography>
                         </Grid>
                     </Grid>
                 </Grid>
                 <Grid item width="100%">
-                    <Typography variant="body2">Here are some of the most favourited recipes of the week</Typography>
+                    <Typography variant="body2">
+                        Here are some of the most favourited recipes of the week
+                    </Typography>
                 </Grid>
                 <Grid item>
-                    
+                    <Grid container spacing={4} justifyContent="center">
+                        {recipes &&
+                            recipes.map((recipe) => (
+                                <Grid xl={3} md={4} sm={6} xs={12} item key={recipe.id}>
+                                    <RecipeItemCard {...recipe} />
+                                </Grid>
+                            ))}
+                    </Grid>
                 </Grid>
             </Grid>
         </Container>
