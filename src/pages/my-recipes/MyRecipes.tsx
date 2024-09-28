be562e420f98722603239742a67405d622540af4
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-09-28 15:33:49 +0800

    Create skeleton of MyRecipes page to render a button that links to Add Recipe

--- src/pages/my-recipes/MyRecipes.tsx
+++ src/pages/my-recipes/MyRecipes.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import { Button, Container, Divider, Grid, Typography } from "@mui/material";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-interface MyRecipesProps {
-    // : string;
-}
+interface MyRecipesProps {}
 
-const MyRecipes: React.FC<MyRecipesProps> = ({  }) => {
+const MyRecipes: React.FC<MyRecipesProps> = ({}) => {
+    const navigate = useNavigate();
+
     return (
-        <>
-            My Recipes
-        </>
+        <Container maxWidth="xl" sx={{ my: 4 }}>
+            <Grid
+                container
+                direction="row"
+                width="100%"
+                justifyContent="space-between"
+                alignItems="center"
+            >
+                <Typography variant="h3">My Recipes</Typography>
+                <Grid item>
+                    <Button
+                        size="large"
+                        variant="contained"
+                        onClick={() => navigate("/myrecipe/add")}
+                    >
+                        Add New Recipe
+                    </Button>
+                </Grid>
+            </Grid>
+            <Divider sx={{ my: 2 }} />
+        </Container>
     );
 };
 
