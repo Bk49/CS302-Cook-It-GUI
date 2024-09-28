a36adbd5bf560c6e1319a81b5b228c7d10fe5ec2
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-09-28 15:32:32 +0800

    Create array of values for cuisine type dropdown options

---
+++ src/constants/Dropdown.ts
@@ -0,0 +1,10 @@
+export const cuisineOptions = [
+    { label: "Italian", value: "Italian" },
+    { label: "Mexican", value: "Mexican" },
+    { label: "Indian", value: "Indian" },
+    { label: "Asian", value: "Asian" },
+    { label: "Mediterranean", value: "Mediterranean" },
+    { label: "American", value: "American" },
+    { label: "African", value: "African" },
+    { label: "French", value: "French" },
+];
