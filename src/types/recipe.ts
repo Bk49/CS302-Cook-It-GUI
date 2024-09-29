8ff0518d336f4335f35ed2712b4132468b5ba88c
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-09-29 16:19:12 +0800

    Create type necessary for displaying various details about a recipe

--- src/types/recipe.ts
+++ src/types/recipe.ts
@@ -7,3 +7,39 @@ export interface RecipeItem {
     rating: number;
     is_favourite: boolean;
 }
+
+export interface RecipeDetails {
+    id: string;
+    name: string;
+    img?: string;
+    author: string;
+    prep_time: number;
+    cook_time: number;
+    rating: number;
+    is_favourite: boolean;
+    created_at: string;
+    description: string;
+    cuisine_type: string;
+    ingredients: IngredientItem[];
+    portion_size: number;
+    steps: PreparationStepItem[];
+    reviews: ReviewItem[];
+}
+
+export interface IngredientItem {
+    ingredient_name: string;
+    quantity: string;
+}
+
+export interface PreparationStepItem {
+    description: string;
+    image?: string;
+}
+
+export interface ReviewItem {
+    name: string;
+    image: string;
+    rating: number;
+    created_at: string;
+    comment: string;
+}
