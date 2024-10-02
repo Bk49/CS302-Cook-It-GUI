76e280f8e11b542d6085e6f5dfdfd9590f211d3c
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-02 16:32:56 +0800

    Configure vite to allow hosting for vite preview

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000
-  },
-  define: {
-    global: {}
-  }
-})
+    plugins: [react()],
+    server: {
+        port: 3000,
+        strictPort: true,
+    },
+    preview: {
+        port: 3000,
+        strictPort: true,
+        host: true,
+    },
+    define: {
+        global: {},
+    },
+});
