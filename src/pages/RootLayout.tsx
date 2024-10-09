c25e7384a0169da8c607c0107c86ad4965875c22
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-09 15:16:43 +0800

    Refactor RootLayout to handle the bug where ApolloClient cannot initialize properly during logout

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,26 +1,31 @@
-import { ApolloProvider } from "@apollo/client";
-import { useAuth0 } from "@auth0/auth0-react";
-import React, { useMemo } from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import { createApolloClient } from "../apollo/client";
 import AppBar from "../components/common/nav/AppBar";
+import { useAuth0 } from "@auth0/auth0-react";
+import { from, useApolloClient } from "@apollo/client";
+import { createAuthLink, httpLink } from "../apollo/client";
 
-interface RootLayoutProps {
-}
+interface RootLayoutProps {}
 
 const RootLayout: React.FC<RootLayoutProps> = ({}) => {
     const { isAuthenticated, getAccessTokenSilently } = useAuth0();
+    const client = useApolloClient();
 
-    const client = useMemo(
-        () => createApolloClient(getAccessTokenSilently),
-        [getAccessTokenSilently, isAuthenticated]
-    );
+    useEffect(() => {
+        if (isAuthenticated) {
+            getAccessTokenSilently().then((token) =>
+                client.setLink(from([createAuthLink(token), httpLink]))
+            );
+        } else {
+            client.setLink(from([createAuthLink(), httpLink]));
+        }
+    }, [isAuthenticated, getAccessTokenSilently]);
 
     return (
-        <ApolloProvider client={client}>
+        <>
             <AppBar />
             <Outlet />
-        </ApolloProvider>
+        </>
     );
 };
 
