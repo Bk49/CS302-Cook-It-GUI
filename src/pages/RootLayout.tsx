2a7d3298a7fc42e4bf036e6b7b29ba00debc0f2c
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-09 12:09:12 +0800

    Configure Apollo Client based on access token changes

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -1,33 +1,26 @@
+import { ApolloProvider } from "@apollo/client";
 import { useAuth0 } from "@auth0/auth0-react";
-import { InternalAxiosRequestConfig } from "axios";
-import React, { useEffect } from "react";
+import React, { useMemo } from "react";
 import { Outlet } from "react-router-dom";
-import { graphQL } from "../axios/client";
+import { createApolloClient } from "../apollo/client";
 import AppBar from "../components/common/nav/AppBar";
 
 interface RootLayoutProps {
-    // : string;
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({}) => {
     const { isAuthenticated, getAccessTokenSilently } = useAuth0();
 
-    useEffect(() => {
-        if (isAuthenticated) {
-            const configureInstance = async (
-                config: InternalAxiosRequestConfig
-            ) => {
-                config.headers.Authorization = `Bearer ${await getAccessTokenSilently()}`;
-                return config;
-            };
-            graphQL.interceptors.request.use(configureInstance);
-        }
-    }, [isAuthenticated]);
+    const client = useMemo(
+        () => createApolloClient(getAccessTokenSilently),
+        [getAccessTokenSilently, isAuthenticated]
+    );
+
     return (
-        <>
+        <ApolloProvider client={client}>
             <AppBar />
             <Outlet />
-        </>
+        </ApolloProvider>
     );
 };
 
