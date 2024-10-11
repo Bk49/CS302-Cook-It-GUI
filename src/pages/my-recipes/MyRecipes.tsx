6d4c712a211c1a8eebc130584eb20e34694b122c
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-11 18:36:36 +0800

    Complete MyRecipes page with MyRecipesListingSection

--- src/pages/my-recipes/MyRecipes.tsx
+++ src/pages/my-recipes/MyRecipes.tsx
@@ -1,6 +1,7 @@
 import { Button, Container, Divider, Grid, Typography } from "@mui/material";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import MyRecipesListingSection from "../../components/recipe/listing/section/MyRecipesListingSection";
 
 interface MyRecipesProps {}
 
@@ -28,6 +29,7 @@ const MyRecipes: React.FC<MyRecipesProps> = ({}) => {
                 </Grid>
             </Grid>
             <Divider sx={{ my: 2 }} />
+            <MyRecipesListingSection />
         </Container>
     );
 };
