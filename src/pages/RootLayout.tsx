c52a864377e6cd1dabb975dc440ce5d3b8ca371b
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-23 19:56:19 +0800

    Addd axios interceptor after authorization in RootLayout

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -4,6 +4,8 @@ import AppBar from "../components/common/nav/AppBar";
 import { useAuth0 } from "@auth0/auth0-react";
 import { from, useApolloClient } from "@apollo/client";
 import { createAuthLink, httpLink } from "../apollo/client";
+import { axiosClient } from "../axios/client";
+import { InternalAxiosRequestConfig } from "axios";
 
 interface RootLayoutProps {}
 
@@ -13,9 +15,15 @@ const RootLayout: React.FC<RootLayoutProps> = ({}) => {
 
     useEffect(() => {
         if (isAuthenticated) {
-            getAccessTokenSilently().then((token) =>
-                client.setLink(from([createAuthLink(token), httpLink]))
-            );
+            getAccessTokenSilently().then((token) => {
+                client.setLink(from([createAuthLink(token), httpLink]));
+                axiosClient.interceptors.request.use(
+                    (config: InternalAxiosRequestConfig<any>) => {
+                        config.headers.Authorization = `Bearer ${token}`;
+                        return config;
+                    }
+                );
+            });
         } else {
             client.setLink(from([createAuthLink(), httpLink]));
         }
