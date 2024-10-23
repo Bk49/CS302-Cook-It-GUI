0d2a7422f209560c264b5deb2b008e8bed0cb86d
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-23 19:56:40 +0800

    Create Add Recipe mutation function with wrapper hook

---
+++ src/custom-hooks/react-query/useAddRecipe.ts
@@ -0,0 +1,52 @@
+import { useMutation } from "@tanstack/react-query";
+import { axiosClient } from "../../axios/client";
+import { AddRecipeI } from "../../zod-schema/add-recipe";
+import useProfile from "../apollo-query/profile/useProfile";
+import { useEffect } from "react";
+
+const useAddRecipe = () => {
+    const [getProfile, { data: profile }] = useProfile();
+
+    useEffect(() => {
+        getProfile();
+    }, [getProfile]);
+
+    return useMutation({
+        mutationFn: async ({
+            steps,
+            prep_time,
+            cook_time,
+            img,
+            ...rest
+        }: AddRecipeI) => {
+            const formData = new FormData();
+
+            // Append main data
+            formData.append(
+                "recipe",
+                JSON.stringify({
+                    ...rest,
+                    author: profile?.id ?? "",
+                    prep_time: prep_time.hrs * 60 + prep_time.mins,
+                    cook_time: cook_time.hrs * 60 + cook_time.mins,
+                    steps: steps.map(({ description }) => ({
+                        description,
+                    })),
+                })
+            );
+
+            // Append image
+            if (img) formData.append("main_image", img);
+
+            steps.forEach(({ image }, index) => {
+                if (image) formData.append(`step_image_${index}`, image);
+            });
+
+            const { data } = await axiosClient.post("api/v1/recipe", formData);
+
+            return data;
+        },
+    });
+};
+
+export default useAddRecipe;
