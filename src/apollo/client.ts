457605e8cb29e1e68e4c32821396ee4a6cfe9f54
Eric Ng <eric.ng.2022@smu.edu.sg>
2024-10-29 19:48:12 +0800

    Add in debugging comments in Apollo Client to disable cache during development

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,4 +1,9 @@
-import { ApolloClient, createHttpLink, from, InMemoryCache } from "@apollo/client";
+import {
+    ApolloClient,
+    createHttpLink,
+    from,
+    InMemoryCache,
+} from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 
 export const httpLink = createHttpLink({
@@ -20,6 +25,16 @@ const authLink = createAuthLink("");
 const client = new ApolloClient({
     cache: new InMemoryCache(),
     link: from([authLink, httpLink]),
+    // defaultOptions: {
+    //     watchQuery: {
+    //         fetchPolicy: "no-cache",
+    //         errorPolicy: "ignore",
+    //     },
+    //     query: {
+    //         fetchPolicy: "no-cache",
+    //         errorPolicy: "all",
+    //     },
+    // },
 });
 
-export default client
+export default client;
